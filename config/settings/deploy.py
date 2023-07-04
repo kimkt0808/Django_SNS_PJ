b885c4018b41c1356a020c31598168e0128f5839
8azelnut <kookb20000@naver.com>
2023-07-04 14:59:35 +0900

    39_deploy.py 수정4

--- config/settings/deploy.py
+++ config/settings/deploy.py
@@ -1,5 +1,15 @@
 from .base import *
 
+
+def get_secret(secret_name):
+    file = open("/run/secrets/" + secret_name)
+    secret = file.read()
+    secret = secret.rstrip().lstrip()
+    file.close()
+
+    return secret
+
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
@@ -13,7 +23,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str("SECRET_KEY")
+SECRET_KEY = get_secret("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -29,7 +39,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "django",
         "USER": "django",
-        "PASSWORD": "password1234",
+        "PASSWORD": get_secret("MYSQL_PASSWORD"),
         "HOST": "mariadb",
         "PORT": "3306",
     }
